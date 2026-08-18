344470a18922f13a8badadd674947f69746b5e76
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2026-08-18 13:08:42 -0700

    test deploy setup

---
+++ _apiKey.js
@@ -0,0 +1,10 @@
+const apiKey = "234567_Paste_Your_Own_API_Key_Here_7654321";
+const box = "https://rhq.ai";
+// const name = "pad";
+
+const appId = "org.tinlizzie.moonlander";
+const tps = 60;
+const eventRateLimit = 60;
+
+export default {appParameters: {apiKey, box, appId, tps, eventRateLimit}};
+
